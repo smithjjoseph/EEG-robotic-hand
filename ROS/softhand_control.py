d19022bd28b7811738055545066dd7c3b01394f5
Joseph Smith <smithjjosephj@gmail.com>
2024-04-06 23:41:53 +0100

    Add comments

--- ROS/softhand_control.py
+++ ROS/softhand_control.py
@@ -1,4 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+"""
+:file: softhand_contol.py
+
+:brief: Contains command line functions for opening and closing a qb softhand 2
+
+:TODO::
+- Consolidate magic values to more useful constants
+  - Test with provided GUI and then use values in script
+- Find out what JointTrajectoryPoint().time_from_start does
+"""
 
 import sys
 import rospy
@@ -8,30 +19,43 @@ from trajectory_msgs.msg import JointTrajectory
 from trajectory_msgs.msg import JointTrajectoryPoint
 
 
-def close():
+def close() -> None:
+    """
+    Sends message to close softhand
+
+    :returns: `None`
+    """
+    # Declares that messages of type JointTrajectory are being posted to a specific topic with a limit of 10 async messages at a time
     pub = rospy.Publisher('/qbhand2m1/control/qbhand2m1_synergies_trajectory_controller/command', JointTrajectory, queue_size=10)
     # Creates a node called 'close_hand' to run process on
     rospy.init_node('close_hand')
+    # Sets the sleep to fulfill a rate of 5Hz
     rate = rospy.Rate(5)
     for step in np.linspace(0, 3.142, 5):
         cmd = JointTrajectory()
         cmd.header.stamp = rospy.Time.now()
         cmd.joint_names = ['qbhand2m1_manipulation_joint', 'qbhand2m1_synergy_joint']
         pt = JointTrajectoryPoint()
-        # pt.positions = [0.0, 0.33*(cos(step+3.142)+1.01)]
         pt.positions = [0.0, 0.37*(cos(step+3.142)+1.01)]
         pt.velocities = [0.0, 0.0]
         pt.accelerations = [0.0, 0.0]
         pt.effort = [0.0, 0.0]
         pt.time_from_start = rospy.Time.from_sec(1)
         cmd.points.append(pt)
-        # rospy.loginfo(cmd)
         pub.publish(cmd)
-        # print(pt.positions)
         rate.sleep()
 
+        # pt.positions = [0.0, 0.33*(cos(step+3.142)+1.01)]
+        # rospy.loginfo(cmd)
+        # print(pt.positions)
 
-def open():
+
+def open() -> None:
+    """
+    Sends message to open softhand
+
+    :returns: `None`
+    """
     pub = rospy.Publisher('/qbhand2m1/control/qbhand2m1_synergies_trajectory_controller/command', JointTrajectory, queue_size=10)
     # Creates a node called 'open_hand' to run process on
     rospy.init_node('open_hand')
@@ -47,14 +71,15 @@ def open():
         pt.effort = [0.0, 0.0]
         pt.time_from_start = rospy.Time.from_sec(1)
         cmd.points.append(pt)
-        # rospy.loginfo(cmd)
         pub.publish(cmd)
-        # print(pt.positions)
         rate.sleep()
+
+        # rospy.loginfo(cmd)
+        # print(pt.positions)
 
 
 def main() -> None:
-    action = sys.argv[1]
+    action: str = sys.argv[1]
     if action == 'close':
         try:
             close()
